def2c65f38efe01d683171fce0c758b99ca030fa
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-10-02 23:28:15 +0600

    Initial

---
+++ index.js
@@ -0,0 +1,16 @@
+{
+  "name": "multiple-api",
+  "version": "1.0.0",
+  "description": "Express API to generate custom animations (GoatBot style)",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "author": "Gtajisan",
+  "license": "MIT",
+  "dependencies": {
+    "axios": "^1.4.0",
+    "express": "^4.18.2",
+    "fs": "^0.0.1-security"
+  }
+}
